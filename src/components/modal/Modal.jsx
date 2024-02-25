af3d5abb14aeacb885e132ab10ced4fa36031d1c
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-02-25 14:40:49 +0100

    remove useCallback in modal component

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,28 +1,29 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import '../styles.css';
 import { createPortal } from 'react-dom';
 
 const modalRoot = document.getElementById('modal-root');
 
 const Modal = ({ children, toggleModal }) => {
-  const closeModal = useCallback(
-    e => {
-      if (e.code === 'Escape') toggleModal();
-      if (e.target === e.currentTarget) toggleModal();
-    },
-    [toggleModal]
-  );
-
   useEffect(() => {
+    const closeModal = e => {
+      if (e.code === 'Escape') toggleModal();
+    };
+
     window.addEventListener('keydown', closeModal);
 
     return () => {
       window.removeEventListener('keydown', closeModal);
     };
-  }, [closeModal]);
+  }, [toggleModal]);
+
+  const overlayClick = e => {
+    if (e.target === e.currentTarget) toggleModal();
+    return;
+  };
 
   return createPortal(
-    <div className="Overlay" onClick={closeModal}>
+    <div className="Overlay" onClick={overlayClick}>
       <div className="Modal">{children}</div>
     </div>,
     modalRoot
